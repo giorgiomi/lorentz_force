616751ab4c2a7c47b0153c51e7b06e544ec36384
giorgiomi <giorgio.micaglio@studenti.unitn.it>
2023-05-26 18:00:57 +0200

    add data

--- lorentz.m
+++ lorentz.m
@@ -1,8 +1,9 @@
-%PAF script
+%% Lorentz force measure
 clear; close all; clc;
 addpath scripts/;
 g = 9.806;
 
+%% Calibrazione sensore di forza
 RSG_verde_giallo = 0.99855e03;
 R_rosso_nero = 1.05177e03;
 V_BIAS = 12;
@@ -23,7 +24,7 @@ errorbar(F,V,dV,'.','MarkerSize',12);
 xlabel("force [N]");
 ylabel("voltage [V]")
 
-fit_data = regressione_lineare(F,V,dV)
+fit_data = regressione_lineare(F,V,dV);
 
 xx = (0:0.1e-03:25e-03)*g;
 plot(xx,fit_data.m*xx+fit_data.b);
@@ -32,9 +33,40 @@ fatt_conv = fit_data.m; %in V/N
 dfatt_conv = fit_data.dm;
 fprintf("Fattore di conversione: %.4f +/- %.5f V/N\n",fatt_conv,dfatt_conv);
 
-%data from DMM
-%{
-dataDMM = readmatrix("defbuffer1.csv");
+
+%% Accoppiamento in DC
+rr = 4.65; %resistenza bobina ricevitrice in ohm
+rs = 4.64; %resistenza bobina sorgente in ohm
+
+dataV = readmatrix("voltage_sensor.csv");
+dataI = readmatrix("current_sorg.csv");
 figure();
-plot(dataDMM(:,1),dataDMM(:,2),'.');
-%}
+hold on
+grid on
+a = 2600;
+b = 5500;
+t = dataV(:,1); t = t(a:b);
+V = dataV(:,2); V = V(a:b);
+tI = dataI(:,1); tI = tI(2.34e4:end);
+I = dataI(:,2); I = I(2.34e4:end);
+plot(t,V,'.-');
+
+figure();
+hold on;
+grid on;
+plot(tI,I,'.-');
+
+
+%% Studio dalla dipendenza da VDC
+d = 1.115e-02; %in cm
+VDC = [6.113 5.114 4.112 3.110 2.106];
+
+
+
+
+
+
+
+
+
+
